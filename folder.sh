bae373370a13fd901d1e3debd918a6a632098472
lorios <rios123lourdes@gmail.com>
2024-11-01 09:15:04 -0300

    Changes in dockerfile

--- folder.sh
+++ folder.sh
@@ -2,3 +2,7 @@
 mkdir -p /opt/airflow/dags/files/webscraper 
 mkdir -p /opt/airflow/dags/files/preprocessed
 chmod -R 777 /opt/airflow/dags/files
+mkdir -p /opt/airflow/logs
+mkdir -p /opt/airflow/plugins
+chmod -R 777 /opt/airflow/logs
+chmod -R 777 /opt/airflow/plugins
